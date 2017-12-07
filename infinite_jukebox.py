572c7eb63d5cbea9a277d0cce8b2676de1fbb674
lebronjames4 <31781917+lebronjames4@users.noreply.github.com>
2017-12-07 06:47:12 -0500

    again, use old fixed version

--- infinite_jukebox.py
+++ infinite_jukebox.py
@@ -1,18 +1,15 @@
 """infinite_jukebox.py - (c) 2017 - Dave Rensin - dave@rensin.com
 
 An attempt to re-create the amazing Infinite Jukebox (http://www.infinitejuke.com)
-created by Paul Lamere of Echo Nest. Uses the Remixatron module to do most of the
+created by Paul Lemere of Echo Nest. Uses the Remixatron module to do most of the
 work.
 
 """
 
 import argparse
 import curses
-import curses.textpad
-import numpy as np
 import os
 import pygame
-import soundfile as sf
 import sys
 import time
 
@@ -23,7 +20,7 @@ def process_args():
 
     """ Process the command line args """
 
-    description = """Creates an infinite remix of an audio file by finding musically similar beats and computing a randomized play path through them. The default choices should be suitable for a variety of musical styles. This work is inspired by the Infinite Jukebox (http://www.infinitejuke.com) project creaeted by Paul Lamere (paul@spotify.com)"""
+    description = """Creates an infinite remix of an audio file by finding musically similar beats and computing a randomized play path through them. The default choices should be suitable for a variety of musical styles. This work is inspired by the Infinite Jukebox (http://www.infinitejuke.com) project creaeted by Paul Lemere (paul@echonest.com)"""
 
     epilog = """
     """
@@ -33,20 +30,11 @@ def process_args():
     parser.add_argument("filename", type=str,
                         help="the name of the audio file to play. Most common audio types should work. (mp3, wav, ogg, etc..)")
 
-    parser.add_argument("-clusters", metavar='N', type=int, default=0,
+    parser.add_argument("-clusters", type=int, default=0,
                         help="set the number of clusters into which we want to bucket the audio. Deafult: 0 (automatically try to find the optimal cluster value.)")
 
-    parser.add_argument("-start", metavar='start_beat', type=int, default=1,
-                        help="start on a specific beat. Deafult: 1")
-
-    parser.add_argument("-save", metavar='label', type=str,
-                        help="Save the remix to a file, rather than play it. Will create file named [label].wav")
-
-    parser.add_argument("-duration", metavar='seconds', type=int, default=180,
-                        help="length (in seconds) to save. Must use with -save. Deafult: 180")
-
-    parser.add_argument("-verbose", action='store_true',
-                        help="print extra info about the track and play vector")
+    parser.add_argument("-start", type=int, default=1,
+                        help="start on beat N. Deafult: 1")
 
     return parser.parse_args()
 
@@ -58,12 +46,14 @@ def MyCallback(pct_complete, message):
         Example: [######    ] Doing some thing...
     """
 
+    curses.setupterm()
+    term_width = curses.tigetnum('cols')
+
     progress_bar = " [" + "".ljust(int(pct_complete * 10),'#') + "".ljust(10 - int(pct_complete * 10), ' ') + "] "
     log_line =  progress_bar + message
 
-    window.clear()
-    window.addstr(1,0,log_line)
-    window.refresh()
+    sys.stdout.write( log_line.ljust(term_width) + '\r' )
+    sys.stdout.flush()
 
 
 def display_playback_progress(v):
@@ -71,7 +61,11 @@ def display_playback_progress(v):
     """
         Displays a super low-fi playback progress map
 
-        See README.md for details..
+        Example:  .............[16].....................
+
+        The dots are the measures in the song. The *number* is a countdown of how
+        many beats until a possible jump. The *location* of the number
+        is the currently playing beat.
 
         Returns the time this function took so we can deduct it from the
         sleep time for the beat
@@ -79,176 +73,59 @@ def display_playback_progress(v):
 
     time_start = time.time()
 
+    curses.setupterm()
     term_width = curses.tigetnum('cols')
 
-    y_offset = 11
+    v_idx = max(0, jukebox.play_vector.index(v) - 1)
+    beat = jukebox.play_vector[v_idx]['beat']
+    min_sequence = jukebox.play_vector[v_idx]['seq_len']
+    current_sequence = jukebox.play_vector[v_idx]['seq_pos']
 
-    beat = v['beat']
-    min_sequence = v['seq_len']
-    current_sequence = v['seq_pos']
+    pct = float(beat) / len(jukebox.beats)
+    term_pos = int( pct * term_width )
 
-    # compute a segment map and display it. See README.md for an
-    # explanation of segment maps and cluster maps.
+    prefix = "".ljust( min(term_pos, term_width - 4), '.') + '[' + str(min_sequence - current_sequence).zfill(2) + ']'
+    log_line =  prefix + "".ljust(term_width - len(prefix), '.')
 
-    segment_map = ''
-    segment_chars = '#-'
-
-    for b in jukebox.beats:
-        segment_map += segment_chars[ b['segment'] % 2 ]
-
-    window.addstr(y_offset,0,segment_map + " ")
-
-    # highlight all the jump candidates in the segment
-    # map
-
-    for c in jukebox.beats[beat]['jump_candidates']:
-
-        b = jukebox.beats[c]
-
-        window.addch(y_offset + int(b['id'] / term_width),   # y position of character
-                      b['id'] % term_width,                  # x position of character
-                      ord(segment_chars[b['segment'] %2]),   # either '#' or '-' depending on the segment
-                      curses.A_REVERSE)                      # print in reverse highlight
-
-    # print the position tracker on the segment map
-
-    x_pos = beat % term_width
-    y_pos = int(beat/term_width) + y_offset
-
-    beats_until_jump = min_sequence - current_sequence
-
-    buj_disp = ''
-
-    # show the beats until the next jump. If the value == 0 then
-    # then sequence wanted to jump but couldn't find a suitable
-    # target. Display an appropriate symbol for that (a frowny face, of course!)
-
-    if beats_until_jump > 0:
-        buj_disp = str(beats_until_jump).zfill(2)
-    else:
-        buj_disp = ':('
-
-    window.addstr(y_pos, x_pos, buj_disp, curses.A_BOLD | curses.A_REVERSE | curses.A_STANDOUT )
-
-    window.refresh()
+    sys.stdout.write( log_line.ljust(term_width) + '\r' )
+    sys.stdout.flush()
 
     time_finish = time.time()
 
     return time_finish - time_start
 
-def get_verbose_info():
+def show_verbose_info():
     """Show statistics about the song and the analysis"""
 
     info = """
+
     filename: %s
-    duration: %02d:%02d:%02d
+    duration: %f seconds
        beats: %d
-       tempo: %d bpm
+       tempo: %f beats per minute
     clusters: %d
-    segments: %d
   samplerate: %d
     """
 
-    (minutes,seconds) = divmod(round(jukebox.duration),60)
-    (hours, minutes)  = divmod(minutes, 60)
-
-    verbose_info = info % (os.path.basename(args.filename), hours, minutes, seconds,
-                           len(jukebox.beats), int(round(jukebox.tempo)), jukebox.clusters, jukebox.segments,
-                           jukebox.sample_rate)
-
-    segment_map = ''
-    cluster_map = ''
-
-    segment_chars = '#-'
-    cluster_chars = 'A1b2c3D4e5F6G7h8I9j0kLMnoPQrsTuVwXyZ~!@#$%^&*()_+-='
-
-    for b in jukebox.beats:
-        segment_map += segment_chars[ b['segment'] % 2 ]
-        cluster_map += cluster_chars[ b['cluster'] ]
-
-    verbose_info += "\n" + segment_map + "\n\n"
-
-    if args.verbose:
-        verbose_info += cluster_map + "\n\n"
-
-    verbose_info += jukebox._extra_diag
-
-    return verbose_info
-
-def get_window_contents():
-    """Dump the contents of the current curses window."""
-
-    tbox = curses.textpad.Textbox(window)
-    tbox.stripspaces = False;
-
-    w_str = tbox.gather()
-
-    return w_str
-
-def cleanup():
-    """Cleanup before exiting"""
-
-    if not window:
-        return
-
-    w_str = get_window_contents()
-    curses.curs_set(1)
-    curses.endwin()
-
-    print(w_str.rstrip())
-    print()
-
-    mixer.quit()
+    print(info % (os.path.basename(args.filename), jukebox.duration,
+                  len(jukebox.beats), jukebox.tempo, jukebox.clusters, jukebox.sample_rate))
 
 if __name__ == "__main__":
 
-    #
-    # Main program logic
-    #
+    """ Main logic """
 
     try:
 
-        window = None
-
         args = process_args()
 
-        curses.setupterm()
-
-        window = curses.initscr()
-        curses.curs_set(0)
+        print()
 
         # do the clustering. Run synchronously. Post status messages to MyCallback()
         jukebox = InfiniteJukebox(filename=args.filename, start_beat=args.start, clusters=args.clusters,
                                   progress_callback=MyCallback, async=False)
 
         # show more info about what was found
-        window.addstr(2,0, get_verbose_info())
-        window.refresh()
-
-        # if we're just saving the remix to a file, then just
-        # find the necessarry beats and do that
-
-        if args.save:
-            avg_beat_duration = 60 / jukebox.tempo
-            num_beats_to_save = int(args.duration / avg_beat_duration)
-
-            # this list comprehension returns all the 'buffer' arrays from the beats
-            # associated with the [0..num_beats_to_save] entries in the play vector
-
-            main_bytes = [jukebox.beats[v['beat']]['buffer'] for v in jukebox.play_vector[0:num_beats_to_save]]
-
-            output_bytes = np.concatenate( main_bytes )
-
-            sf.write(args.save + '.wav', output_bytes, jukebox.sample_rate, subtype='PCM_24')
-
-            w_str = get_window_contents()
-            curses.curs_set(1)
-            curses.endwin()
-
-            print(w_str.rstrip())
-            print()
-
-            sys.exit()
+        show_verbose_info()
 
         # important to make sure the mixer is setup with the
         # same sample rate as the audio. Otherwise the playback will
@@ -273,6 +150,6 @@ if __name__ == "__main__":
             pygame.time.wait( int( (beat_to_play['duration'] - how_long_this_took) * 1000 ) )
 
     except KeyboardInterrupt:
-        cleanup()
-    finally:
-        cleanup()
+        print()
+        print('exiting...')
+        mixer.quit()
